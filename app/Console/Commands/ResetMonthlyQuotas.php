7317d980fe2ccec308320306ceaf8f7578a9a66b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-04-07 20:08:12 +0200

    Update ResetMonthlyQuotas.php

--- app/Console/Commands/ResetMonthlyQuotas.php
+++ app/Console/Commands/ResetMonthlyQuotas.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Smtp;
-use App\Models\SmtpHistory;
+use App\Models\SmtpUsageHistory;
 use Carbon\Carbon;
 
 class ResetMonthlyQuotas extends Command
