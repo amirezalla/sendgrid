116f5c0b5024ec14dc83d3e3e0ef2ee09806e8da
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-04-07 16:19:42 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('quotas:reset')->everyMinute();
+        $schedule->command('quotas:reset')->monthlyOn(1, '00:00');
         // $schedule->command('inspire')->hourly();
     }
 
